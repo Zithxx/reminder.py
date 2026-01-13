bd8e83f5e9f001629ed02c0644884a80a765c39d
Zithxx <rodrigo.orlando.nogueira@gmail.com>
2026-01-13 23:37:37 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import requests
 # Final configuration
 phone_number = '+351913221808'
 image_url = 'https://i.postimg.cc/NFmkX9LC/1768346889340.png'
-message_text = f'This is a reminder to take your vitamins 🩷 Love you 🩷 {image_url}'
+message_text = f'Reminder to take your vitamins! Love you. {image_url}'
 
 def send_text():
     response = requests.post('https://textbelt.com/text', {
-        'number': phone_number,   # Make sure this is 'number'
+        'number': phone_number,
         'message': message_text,
         'key': 'textbelt',
     })
